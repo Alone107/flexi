d086d986f5a9f6a14b7002b0a52650c72606439e
Alone107 <123736524+Alone107@users.noreply.github.com>
2023-06-27 16:54:27 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,83 @@
+$('.nine-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  dots: true,
+});
+
+$('.eleven-slider').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  responsive: [
+    {
+      breakpoint: 940,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 3,
+        infinite: true,
+      }},]
+});
+
+
+document.querySelector('.ten-block-title').onclick = () => {
+  
+  document.querySelector('.text-2').classList.remove('text-none')
+  document.querySelector('.text-3').classList.remove('text-none')
+  document.querySelector('.text-4').classList.remove('text-none')
+
+  document.querySelector('.title-3').classList.remove('rotate')
+  document.querySelector('.title-2').classList.remove('rotate')
+  document.querySelector('.title-4').classList.remove('rotate')
+
+
+  document.querySelector('.text-1').classList.toggle('text-none')
+  document.querySelector('.title').classList.toggle('rotate')
+}
+document.querySelector('.title-2').onclick = () => {
+
+  document.querySelector('.text-1').classList.remove('text-none')
+  document.querySelector('.text-3').classList.remove('text-none')
+  document.querySelector('.text-4').classList.remove('text-none')
+
+  document.querySelector('.title-3').classList.remove('rotate')
+  document.querySelector('.title-4').classList.remove('rotate')
+  document.querySelector('.title').classList.remove('rotate')
+
+  document.querySelector('.text-2').classList.toggle('text-none')
+  document.querySelector('.title-2').classList.toggle('rotate')
+
+}
+document.querySelector('.title-3').onclick = () => {
+  document.querySelector('.text-1').classList.remove('text-none')
+  document.querySelector('.text-2').classList.remove('text-none')
+  document.querySelector('.text-4').classList.remove('text-none')
+
+  document.querySelector('.title-4').classList.remove('rotate')
+  document.querySelector('.title-2').classList.remove('rotate')
+  document.querySelector('.title').classList.remove('rotate')
+
+  document.querySelector('.text-3').classList.toggle('text-none')
+  document.querySelector('.title-3').classList.toggle('rotate')
+
+}
+document.querySelector('.title-4').onclick = () => {
+  document.querySelector('.text-1').classList.remove('text-none')
+  document.querySelector('.text-3').classList.remove('text-none')
+  document.querySelector('.text-2').classList.remove('text-none')
+
+  document.querySelector('.title-3').classList.remove('rotate')
+  document.querySelector('.title-2').classList.remove('rotate')
+  document.querySelector('.title').classList.remove('rotate')
+
+
+  document.querySelector('.text-4').classList.toggle('text-none')
+  document.querySelector('.title-4').classList.toggle('rotate')
+
+}
+
+// title.forEach((element) => {
+//   element.classList.remove('active');
+// });
